aba08bd5a2f37b41ffe278aafa3ceef5d2a28313
Thijs Vogels <t.vogels@me.com>
2014-07-02 10:14:35 +0200

    Curl with authentication in Ask API & parameters

--- src/AskApi.php
+++ src/AskApi.php
@@ -14,17 +14,24 @@ class AskApi {
 
   private $host;
 
-  public function __construct($host) {
+  public function __construct($host, $user, $pass) {
 
     $this->host = $host;
-  
+    $this->user = $user;
+    $this->pass = $pass;
+
   }
 
   public function query($q) {
     $url = $this->host . '/api.php';
-    $response = json_decode(
-      file_get_contents("http://{$url}?action=ask&format=json&query=" . urlencode($q . "|limit=5000"))
-    );
+
+    $curl = curl_init("http://{$url}?action=ask&format=json&query=" . urlencode($q . "|limit=5000");
+    if ($this->user != '') {
+      curl_setopt($curl, CURLOPT_USERPWD, $this->user . ":" . $this->pass);
+    }
+    $response = curl_exec($curl);
+    curl_close($curl);
+
     $output = array();
     foreach ($response->query->results as $result) {
       $output[] = $result;
