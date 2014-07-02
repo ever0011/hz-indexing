df7ed4467da4798894eccb28d9cee641f0921d9c
Thijs Vogels <t.vogels@me.com>
2014-07-02 15:52:48 +0200

    Resource descriptions neatly included

--- src/Command/ResourceDescriptionsCommand.php
+++ src/Command/ResourceDescriptionsCommand.php
@@ -27,71 +27,6 @@ class ResourceDescriptionsCommand extends Command
 {
 
     /**
-     * Private dictionary with extensions and their type
-     */
-    private $extensions_map = array(
-        "jpeg" => "image",
-        "jpg" => "image",
-        "png" => "image",
-        "gif" => "image",
-        "bmp" => "image",
-        "pdf" => "pdf",
-        "doc" => "ms-word",
-        "docx" => "ms-word",
-        "xls" => "ms-excel",
-        "xlsx" => "ms-excel",
-        "ppt" => "ms-powerpoint",
-        "pptx" => "ms-powerpoint"
-    );
-
-    /**
-     * This maps extensions to their type
-     * If the type is unknown, it is seen as a type of its own
-     * 
-     * @param string $extension
-     * 
-     * @return string output file type
-     */
-    private function mapExtension($extension)
-    {
-        if(array_key_exists($extension, $this->extensions_map)) {
-            return $this->extensions_map[$extension];
-        } else {
-            return $extension;
-        }
-    }
-
-    /**
-     * This returns an instance of the appropriate reader class
-     * for a certain file type.
-     * 
-     * @param string $type File type (defined in $extensions_map)
-     * @param string $fullpath Path to the file
-     * 
-     * @return FileReader file reader object.
-     */
-    private function getReader($type, $fullpath)
-    {
-        switch ($type) {
-            case 'image':
-                return new FileReader\ImageReader($fullpath);
-                break;
-            case 'pdf':
-                return new FileReader\PdfReader($fullpath);
-                break;
-            case 'ms-word':
-                return new FileReader\MsWordReader($fullpath);
-                break;
-            case 'ms-powerpoint':
-                return new FileReader\MsPowerPointReader($fullpath);
-                break;
-            default:
-                return new FileReader($fullpath);
-                break;
-        }
-    }
-
-    /**
      * This configures the command.
      */
     protected function configure()
@@ -111,118 +46,34 @@ class ResourceDescriptionsCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         global $container;
-        $es = $container->get('elasticsearch');
         $ask = $container->get('ask');
-        $formatter = $container->get('formatter');
+        $indexer = $container->get('indexer.resourcedescription');
 
-        $output->writeln('<bg=yellow;options=bold>Add resource descriptions to the index</bg=yellow;options=bold>');
+        $output->writeln('<bg=yellow;options=bold>Add Resource descriptions to the index</bg=yellow;options=bold>');
 
-        # Load SKOS Concepts via ASK
-        $output->writeln('- Loading resource descriptions (ASK) ...');
-        $out = $ask->query('
-        [[Category:Resource Description]]
-        [[Dct:title::+]]
-        |?Dct:title
-        |?Dct:creator
-        |?Dct:date
-        |?Dct:description
-        |?Dct:subject
-        |?Hyperlink
-        ');
-        $elements = $out->getResults();
-        $n = count($elements);
-        $output->writeln("  $n found.");
+        # Load concepts via Ask
+        $output->writeln('- Loading concepts (ASK) ...');
+        $concepts = $ask->query('[[Category:Resource Description]][[Dct:title::+]]')->getResults();
+        $n = count($concepts);
+        $output->writeln(sprintf("  %d found.", $n));
 
+        # Set up a progress indicator
         $output->writeln("- Creating index ...\n");
         $progress = new ProgressBar($output, $n);
         $progress->setMessage('Starting ...');
         $progress->setFormat("  %current%/%max% [%bar%] %percent%% \n  %message%");
         $progress->start();
 
-        $noFileCount = 0;
-        $notFoundCount = 0;
-        foreach ($elements as $c) {
-
-            $progress->setMessage($formatter->prettify($c));
+        # Do the actual indexing
+        foreach ($concepts as $concept) {
+            $progress->setMessage($concept->getName());
+            $indexer->index($concept->getName());
             $progress->advance();
-
-            $filecontents = "";
-            $fullpath = "";
-            $filepath = "";
-            $hyperlink = false;
-            $link = null;
-            // case of a File
-            if (preg_match(
-                "@(Bestand|Media|File):(.*\.([a-z]+))$@i", 
-                $c->getUrl(), 
-                $matches
-            )) {
-
-                $filename = $matches[2];
-                $md5 = md5($filename);
-                $filepath = "/" . $md5[0] . "/" . substr($md5, 0,2) . "/" . $filename;
-                $fullpath = $container->getParameter('upload.dir') . $filepath;
-
-                // determine extension & type
-                $extension = strtolower($matches[3]);
-                $extension_type = $this->mapExtension($extension);
-
-                if (file_exists($fullpath)) {
-                    $reader = $this->getReader($extension_type, $fullpath);
-                    $filecontents = $reader->result();
-                } else {
-                    $notFoundCount++;
-                }
-
-            }
-            // hyperlink
-            elseif (count($c->urls('hyperlink')) > 0)
-            {
-                $hyperlink = true;
-                $link = $c->urls_cs('hyperlink');
-            }
-            else 
-            {
-                $noFileCount++;
-                continue;
-            }
-
-            $autoCompleteInput[] = $c->values('dct_title');
-
-
-            $params['body'] = array(
-                "url" => $c->getUrl(),
-                "title" => $c->values_cs('dct_title'),
-                "creator" => $c->values_cs('dct_creator'),
-                "date" => $c->values_cs('dct_date'),
-                "description" => $c->values_cs('dct_description'),
-                "subject" => $c->urls('dct_subject'),
-                "filename" => $c->getName(),
-                "content" => $filecontents,
-                "context_readable"=> 'TODO',
-                "context"=> 'TODO',
-                "vn_pages" => array(),
-                "suggest" => array(
-                    "input" => $autoCompleteInput,
-                    "output" => $c->getName(),
-                    "payload" => array(
-                        "url" => $c->getUrl(),
-                        "vn_pages" => array(),
-                        "context" => 'TODO',
-                        "type" => 'resource_description'
-                    )
-                )
-            );
-            $params['index'] = $container->getParameter('elastic.index');
-            $params['type'] = 'resource_description';
-            $params['id'] = md5($c->getUrl());
-            $ret = $es->index($params);
         }
 
+        # Finish up
         $progress->setMessage('Done.');
         $progress->finish();
         $output->writeln('');
-        $output->writeln('  <info>Skipped ' . $noFileCount . ' rs\'s that are no file and have no link.</info>');
-        $output->writeln('  <info>' . $notFoundCount . ' files were not found.</info>');
     }
 }
---
+++ src/Indexer/ResourceDescriptionIndexer.php
@@ -0,0 +1,267 @@
+<?php
+
+/**
+ * This file is part of the indexing code for the semantic search engine of
+ * the HzBwNature wiki. 
+ *
+ * It was developed by Thijs Vogels (t.vogels@me.com) for the HZ University of
+ * Applied Sciences.
+ */
+
+namespace TV\HZ\Indexer;
+use TV\HZ\FileReader;
+
+/**
+ * This class is meant to index contexts
+ * 
+ * @author Thijs Vogels <t.vogels@me.com>
+ */
+class ResourceDescriptionIndexer extends IndexerAbstract
+{
+
+    const TYPE = "resource_description";
+
+    /**
+     * Upload dir parameter
+     */
+    private $uploadDir;
+
+    public function __construct($ask, $es, $index, $uploadDir)
+    {
+        $this->ask = $ask;
+        $this->es = $es;
+        $this->index = $index;
+        $this->uploadDir = $uploadDir;
+    }
+
+    /**
+     * This does the actual indexing
+     * 
+     * @param string $name The name of the context to be indexed
+     */
+    public function index($name)
+    {
+        $data = $this->getData($name);
+
+        $filecontents = "";
+        $fullpath = "";
+        $filepath = "";
+        $hyperlink = false;
+        $link = null;
+        // case of a File
+        if (preg_match(
+            "@(Bestand|Media|File):(.*\.([a-z]+))$@i", 
+            $data->getUrl(), 
+            $matches
+        )) {
+
+            $filename = $matches[2];
+            $md5 = md5($filename);
+            $filepath = "/" . $md5[0] . "/" . substr($md5, 0,2) . "/" . $filename;
+            $fullpath = $this->uploadDir . $filepath;
+            
+            // determine extension & type
+            $extension = strtolower($matches[3]);
+            $extension_type = $this->mapExtension($extension);
+
+            if (file_exists($fullpath)) {
+                $reader = $this->getReader($extension_type, $fullpath);
+                $filecontents = $reader->result();
+            } else {
+                $notFoundCount++;
+            }
+
+        }
+        // hyperlink
+        elseif (count($data->urls('hyperlink')) > 0)
+        {
+            $hyperlink = true;
+            $link = $data->urls_cs('hyperlink');
+        }
+        else 
+        {
+            $noFileCount++;
+            continue;
+        }
+
+        $autoCompleteInput[] = $data->values('dct_title');
+
+
+        $params['body'] = array(
+            "url" => $data->getUrl(),
+            "title" => $data->values_cs('dct_title'),
+            "creator" => $data->values_cs('dct_creator'),
+            "date" => $data->values_cs('dct_date'),
+            "description" => $data->values_cs('dct_description'),
+            "subject" => $data->urls('dct_subject'),
+            "filename" => $data->getName(),
+            "content" => $filecontents,
+            "context_readable"=> 'TODO',
+            "context"=> 'TODO',
+            "vn_pages" => array(),
+            "suggest" => array(
+                "input" => $autoCompleteInput,
+                "output" => $data->getName(),
+                "payload" => array(
+                    "url" => $data->getUrl(),
+                    "vn_pages" => array(),
+                    "context" => 'TODO',
+                    "type" => 'resource_description'
+                )
+            )
+        );
+        $params['index'] = $this->index;
+        $params['type'] = self::TYPE;
+        $params['id'] = $this->getPageId($data);
+
+        return $this->es->index($params);
+    }
+
+    /**
+     * This gets the data from ASK
+     * 
+     * @param string $name The name of the context to be indexed
+     * 
+     * @throws \Exception
+     */
+    private function getData($name)
+    {
+        $output = $this->ask->query("
+            [[{$name}]]
+            |?Dct:title
+            |?Dct:creator
+            |?Dct:date
+            |?Dct:description
+            |?Dct:subject
+            |?Hyperlink
+        ")->getResults();
+
+        if (count($output) == 0) {
+            throw new \Exception(sprintf("The page '%s' could not be found", $name));
+        }
+
+        return $output[0];
+    }
+
+    /**
+     * Get the autocomplete input from data
+     * 
+     * @param \TV\HZ\Ask\Entry $data
+     * 
+     * @return string id
+     */
+    public function getAutocompleteInput(\TV\HZ\Ask\Entry $data)
+    {
+        return $data->getName();
+    }
+
+    /**
+     * Get the VN pages for a page
+     * 
+     * @param \TV\HZ\Ask\Entry $data
+     * 
+     * @return array urls
+     */
+    public function findVnPages(\TV\HZ\Ask\Entry $data)
+    {
+        $query = "[[Model link::{$data->getName()}]]";
+        $vns = $this->ask->query($query)->getResults();
+        $vnurls = array();
+        foreach ($vns as $vn) {
+          $vnurls[] = $vn->getUrl();
+        }
+
+        return $vnurls;
+    }
+
+    /**
+     * Get the paragraphs connected to the page
+     * 
+     * @param \TV\HZ\Ask\Entry $data
+     * 
+     * @return array paragraphs
+     */
+    public function findParagraphs(\TV\HZ\Ask\Entry $data)
+    {
+        $query = '[[Paragraph::+]]
+        [[Paragraph back link::'.$data->getName().']]
+        |?Paragraph
+        |?Paragraph subheading
+        |?Paragraph language
+        |?Paragraph number
+        |?Paragraph back link
+        ';
+        $paragraphs = $this->ask->query($query)->getResults();
+        return array_map(function ($p) {
+            return $p->values_cs('paragraph_subheading') . ": " . $p->values_cs('paragraph');
+        }, $paragraphs);
+    }
+
+
+
+    /**
+     * Private dictionary with extensions and their type
+     */
+    private $extensions_map = array(
+        "jpeg" => "image",
+        "jpg" => "image",
+        "png" => "image",
+        "gif" => "image",
+        "bmp" => "image",
+        "pdf" => "pdf",
+        "doc" => "ms-word",
+        "docx" => "ms-word",
+        "xls" => "ms-excel",
+        "xlsx" => "ms-excel",
+        "ppt" => "ms-powerpoint",
+        "pptx" => "ms-powerpoint"
+    );
+
+    /**
+     * This maps extensions to their type
+     * If the type is unknown, it is seen as a type of its own
+     * 
+     * @param string $extension
+     * 
+     * @return string output file type
+     */
+    private function mapExtension($extension)
+    {
+        if(array_key_exists($extension, $this->extensions_map)) {
+            return $this->extensions_map[$extension];
+        } else {
+            return $extension;
+        }
+    }
+
+    /**
+     * This returns an instance of the appropriate reader class
+     * for a certain file type.
+     * 
+     * @param string $type File type (defined in $extensions_map)
+     * @param string $fullpath Path to the file
+     * 
+     * @return FileReader file reader object.
+     */
+    private function getReader($type, $fullpath)
+    {
+        switch ($type) {
+            case 'image':
+                return new FileReader\ImageReader($fullpath);
+                break;
+            case 'pdf':
+                return new FileReader\PdfReader($fullpath);
+                break;
+            case 'ms-word':
+                return new FileReader\MsWordReader($fullpath);
+                break;
+            case 'ms-powerpoint':
+                return new FileReader\MsPowerPointReader($fullpath);
+                break;
+            default:
+                return new FileReader($fullpath);
+                break;
+        }
+    }
+
+}
